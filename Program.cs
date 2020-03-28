f5f8a5dcd516bab2345108da792c177e52141a11
MechaDragonX <theofficialrv@gmail.com>
2020-03-28 12:51:49 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using DiscordRPC;
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Bheithir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(Process.GetProcesses().Where(x => x.ProcessName.StartsWith("DOSBox")).Count() == 0)
+            {
+                Console.WriteLine("DOSBox was not found! Is it open?");
+                return;
+            }
+
+            Process dos = Process.GetProcesses().Where(x => x.ProcessName.StartsWith("DOSBox")).ToList()[0];
+            string currentWindowTitle = dos.MainWindowTitle;
+            using(DiscordRpcClient client = new DiscordRpcClient("693311130856325180"))
+            {
+                try
+                {
+                    client.Initialize();
+                    Console.WriteLine("Successfully connected to client!");
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine($"Connection to client was not successful!\nERROR: {e.Message}");
+                    return;
+                }
+
+                try
+                {
+                    client.SetPresence(new RichPresence
+                    {
+                        Details = currentWindowTitle,
+                        State = "State Placeholder",
+                        Timestamps = new Timestamps(DateTime.UtcNow)
+                    });
+                    Console.WriteLine("Presence successfully set!");
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine($"Setting presence was not successful!\nERROR: {e.Message}");
+                    return;
+                }
+
+                while(true)
+                {
+                    if(Process.GetProcesses().Where(x => x.ProcessName.StartsWith("DOSBox")).Count() == 0)
+                    {
+                        client.ClearPresence();
+                        Console.WriteLine("Thanks for using Bheithir!");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
