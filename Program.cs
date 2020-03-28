e5441697b0873a664a38c4e0912aeb47ae8bcf64
MechaDragonX <theofficialrv@gmail.com>
2020-03-28 13:15:00 -0700

    Prepare for event-based updating

--- Program.cs
+++ Program.cs
@@ -7,7 +7,24 @@ namespace Bheithir
 {
     class Program
     {
+        private static DiscordRpcClient client;
+
         static void Main(string[] args)
+        {
+            Initialize();
+
+            while(true)
+            {
+                if(Process.GetProcesses().Where(x => x.ProcessName.StartsWith("DOSBox")).Count() == 0)
+                {
+                    client.ClearPresence();
+                    Console.WriteLine("Thanks for using Bheithir!");
+                    return;
+                }
+            }
+        }
+
+        static void Initialize()
         {
             if(Process.GetProcesses().Where(x => x.ProcessName.StartsWith("DOSBox")).Count() == 0)
             {
@@ -17,45 +34,36 @@ namespace Bheithir
 
             Process dos = Process.GetProcesses().Where(x => x.ProcessName.StartsWith("DOSBox")).ToList()[0];
             string currentWindowTitle = dos.MainWindowTitle;
-            using(DiscordRpcClient client = new DiscordRpcClient("693311130856325180"))
+
+            client = new DiscordRpcClient("693311130856325180");
+            try
             {
-                try
-                {
-                    client.Initialize();
-                    Console.WriteLine("Successfully connected to client!");
-                }
-                catch(Exception e)
-                {
-                    Console.WriteLine($"Connection to client was not successful!\nERROR: {e.Message}");
-                    return;
-                }
+                client.Initialize();
+                Console.WriteLine("Successfully connected to client!");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Connection to client was not successful!\nERROR: {e.Message}");
+                return;
+            }
 
-                try
+            try
+            {
+                client.SetPresence(new RichPresence
                 {
-                    client.SetPresence(new RichPresence
-                    {
-                        Details = currentWindowTitle,
-                        State = "State Placeholder",
-                        Timestamps = new Timestamps(DateTime.UtcNow)
-                    });
-                    Console.WriteLine("Presence successfully set!");
-                }
-                catch(Exception e)
-                {
-                    Console.WriteLine($"Setting presence was not successful!\nERROR: {e.Message}");
-                    return;
-                }
-
-                while(true)
-                {
-                    if(Process.GetProcesses().Where(x => x.ProcessName.StartsWith("DOSBox")).Count() == 0)
-                    {
-                        client.ClearPresence();
-                        Console.WriteLine("Thanks for using Bheithir!");
-                        return;
-                    }
-                }
+                    Details = currentWindowTitle,
+                    State = "State Placeholder",
+                    Timestamps = new Timestamps(DateTime.UtcNow)
+                });
+                Console.WriteLine("Presence successfully set!");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Setting presence was not successful!\nERROR: {e.Message}");
+                return;
             }
         }
+
+
     }
 }
