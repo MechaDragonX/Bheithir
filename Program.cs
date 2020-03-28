366629b6e64b3ac2f6cb7343953d0eeabc1a5469
MechaDragonX <theofficialrv@gmail.com>
2020-03-28 15:16:21 -0700

    Dynamically change presence and display proper details and status

--- Program.cs
+++ Program.cs
@@ -1,20 +1,29 @@
 ﻿using DiscordRPC;
+using DiscordRPC.Message;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Bheithir
 {
     class Program
     {
         private static DiscordRpcClient client;
+        private static Process dos;
+        private static string windowTitle;
+        private static Regex windowPattern = new Regex("(,\\s)+", RegexOptions.Compiled);
+        // private static string[] titleParts;
+        // private static string presenceDetails;
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Initialize();
-            // Update();
             while(true)
             {
+                Update();
                 if(Process.GetProcesses().Where(x => x.ProcessName.StartsWith("DOSBox")).Count() == 0)
                 {
                     Deinitialize();
@@ -24,7 +33,7 @@ namespace Bheithir
             }
         }
 
-        static void Initialize()
+        private static void Initialize()
         {
             client = new DiscordRpcClient("693311130856325180");
 
@@ -33,8 +42,8 @@ namespace Bheithir
                 Console.WriteLine("DOSBox was not found! Is it open?");
                 return;
             }
-            Process dos = Process.GetProcesses().Where(x => x.ProcessName.StartsWith("DOSBox")).ToList()[0];
-            string currentWindowTitle = dos.MainWindowTitle;
+            dos = Process.GetProcesses().Where(x => x.ProcessName.StartsWith("DOSBox")).ToList()[0];
+            windowTitle = dos.MainWindowTitle;
 
             client.OnReady += (sender, e) =>
             {
@@ -56,32 +65,58 @@ namespace Bheithir
                 return;
             }
 
-            try
-            {
-                client.SetPresence(new RichPresence
-                {
-                    Details = currentWindowTitle,
-                    State = "State Placeholder",
-                    Timestamps = new Timestamps(DateTime.UtcNow)
-                });
-                Console.WriteLine("Presence successfully set!");
-            }
+            try { setNewPresence(); }
             catch(Exception e)
             {
                 Console.WriteLine($"Setting presence was not successful!\nERROR: {e.Message}");
                 return;
             }
         }
-
-        static void Update()
+        private static void Update()
         {
+            client.OnPresenceUpdate += (sender, e) =>
+            {
+                Console.WriteLine("Received Update! {0}", e.Presence);
+            };
             client.Invoke();
+            OnUpdate();
         }
-
-        static void Deinitialize()
+        private static void Deinitialize()
         {
             client.ClearPresence();
             client.Dispose();
+        }
+
+        private static void OnUpdate()
+        {
+            if(Process.GetProcesses().Where(x => x.ProcessName.StartsWith("DOSBox")).ToList()[0].MainWindowTitle != windowTitle)
+            {
+                dos = Process.GetProcesses().Where(x => x.ProcessName.StartsWith("DOSBox")).ToList()[0];
+                windowTitle = dos.MainWindowTitle;
+                setNewPresence();
+            }
+        }
+        private static void setNewPresence()
+        {
+            List<string> titleParts = windowPattern.Split(windowTitle).ToList();
+            for(int i = 0; i < titleParts.Count; i++)
+            {
+                if(titleParts[i] == ", ")
+                {
+                    titleParts.Remove(titleParts[i]);
+                    i--;
+                }
+            }
+            string status = new StringBuilder($"v{titleParts[0].Split(' ')[1]}").AppendFormat(", {0}, {1}", titleParts[1], titleParts[2]).ToString();
+
+
+            client.SetPresence(new RichPresence
+            {
+                Details = Regex.Split(titleParts[3], "\\s+")[1],
+                State = status,
+                Timestamps = new Timestamps(DateTime.UtcNow)
+            });
+            Console.WriteLine("Presence successfully set!");
         }
     }
 }
