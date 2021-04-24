2b43d38108778c9b792723167b93f2c1cd7a7e43
MechaDragonX <theofficalrv@gmail.com>
2021-04-24 15:49:30 -0700

    Remove unncessary usings

--- Program.cs
+++ Program.cs
@@ -1,11 +1,7 @@
-﻿using DiscordRPC;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
 using Bheithir.Emulators;
 
 namespace Bheithir
--- emulators/Presence.cs
+++ emulators/Presence.cs
@@ -1,8 +1,5 @@
 ﻿using DiscordRPC;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Bheithir.Emulators
